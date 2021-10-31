f6a509bf7f861a73bc8f77945d120d0ba14b43ee
Nazario Safariesqi <nazariotyo@gmail.com>
2021-10-31 11:41:58 +0700

    NazarioSafariesqi-1941720233

---
+++ cypress/integration/3-sauce-demo/login1.js
@@ -0,0 +1,8 @@
+context ('Saucedemo Login Test', () =>{
+    it('Login Saucedemo', () =>{
+        cy.visit('https://saucedemo.com')
+        cy.get('[id="user-name"]').type('standard_user')
+        cy.get('input[type="password"]').type('secret_sauce')
+        cy.get('[id="login-button"]').click()
+    })
+})
